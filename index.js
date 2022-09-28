ea9655be4b89f1e8c37318ae105c0573b3d65499
Aaron van den Berg <a.vdberg98@gmail.com>
2022-09-28 17:25:16 +0200

    Test with GH Action

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ try {
   core.setOutput("serverless-apps", JSON.stringify(serverlessApps, undefined, 2));
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context, undefined, 2)
+  const payload = JSON.stringify(github, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
