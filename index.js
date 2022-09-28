2c2e26189d22a19073dd0f188716dda86452405e
Aaron van den Berg <a.vdberg98@gmail.com>
2022-09-28 17:49:34 +0200

    Test with GH Action

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const glob = require('@actions/glob');
 
 try {
   // `apps-directory` input defined in action metadata file
-  const appsDirectory = core.getInput('apps-directory');
+  const appsDirectory = core.getInput('apps-directory', { required: true });
   console.log(`Apps directory is: ${appsDirectory}!`);
 
   // Here we loop through the monorepo and return the path to each app
@@ -12,6 +13,17 @@ try {
     'booking-api',
   ];
   core.setOutput("serverless-apps", JSON.stringify(serverlessApps, undefined, 2));
+
+  core.info('\u001b[35mLooping through files in the monorepo');
+  const findFiles = async () => {
+    const globber = await glob.create('**', { followSymbolicLinks: true })
+    for await (const file of globber.globGenerator()) {
+      console.log(file)
+    }
+  }
+  findFiles().then(() => {
+    console.log('Done looping through files in the monorepo');
+  });
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github, undefined, 2)
