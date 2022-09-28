7a8cb35385a36c866790cc951066362b5cfacc9b
Aaron van den Berg <a.vdberg98@gmail.com>
2022-09-28 17:09:22 +0200

    Test with GH Action

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // `apps-directory` input defined in action metadata file
+  const appsDirectory = core.getInput('apps-directory');
+  console.log(`The directory where the apps are located: ${appsDirectory}!`);
+
+  core.setOutput("apps-directory", appsDirectory);
+
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
