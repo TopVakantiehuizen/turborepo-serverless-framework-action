c03047e7e25349d215fed24d9d066e8bff026284
Aaron van den Berg <a.vdberg98@gmail.com>
2022-10-05 22:08:56 +0200

    Logging output

--- index.js
+++ index.js
@@ -5,30 +5,34 @@ const glob = require('@actions/glob');
 try {
   // `apps-directory` input defined in action metadata file
   const appsDirectory = core.getInput('apps-directory', {required: true});
-  console.log(`Apps directory is: ${appsDirectory}!`);
+  console.log(`Given applications directory is: "${appsDirectory}".`);
 
   /** Uncomment below to return a mocked list of directories **/
-  // core.setOutput("serverless-apps", JSON.stringify(["apps/availability-api"]));
+    // core.setOutput("serverless-apps", JSON.stringify(["apps/availability-api"]));
 
-  // Here we loop through the monorepo and return the path to each Serverless app
+    // Here we loop through the monorepo and return the path to each Serverless app
   const findFiles = async () => {
-    const serverlessApps = [];
-    const repo = github.context.payload.repository.name;
-    const runnerPath = `/home/runner/work/${repo}/${repo}`;
+      const serverlessApps = [];
+      const repo = github.context.payload.repository.name;
+      const runnerPath = `/home/runner/work/${repo}/${repo}`;
+      console.log('repo', repo);
+      console.log('runnerPath', runnerPath);
 
-    const patterns = [`${appsDirectory}/!*!/serverless.yml`, `${appsDirectory}/!*!/serverless.yaml`]
-    const globber = await glob.create(patterns.join('\n'))
-    for await (const file of globber.globGenerator()) {
-      serverlessApps.push(
-        file
-          .replace(runnerPath, '')
-          .replace('serverless.yaml', '')
-          .replace('serverless.yml', '')
-      );
+      const patterns = [`${appsDirectory}/!*!/serverless.yml`, `${appsDirectory}/!*!/serverless.yaml`]
+      console.log('patterns', patterns);
+      const globber = await glob.create(patterns.join('\n'))
+      for await (const file of globber.globGenerator()) {
+        console.log('file', file);
+        serverlessApps.push(
+          file
+            .replace(runnerPath, '')
+            .replace('serverless.yaml', '')
+            .replace('serverless.yml', '')
+        );
+      }
+
+      return serverlessApps;
     }
-
-    return serverlessApps;
-  }
   findFiles().then((serverlessApps) => {
     console.log('Serverless Apps found:', JSON.stringify(serverlessApps, undefined, 2));
     core.setOutput("serverless-apps", JSON.stringify(JSON.stringify(['apps/availability-api'])));
